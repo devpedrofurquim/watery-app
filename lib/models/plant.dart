2bf389a7ee99fd5b259447d37316f99eb0534b4e
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-05-04 21:20:56 -0300

    Added new homepage, models and plant details page

--- lib/models/plant.dart
+++ lib/models/plant.dart
@@ -1,9 +1,14 @@
 class Plant {
   final String name;
   final String imageUrl;
+  final String wateringSchedule;
+  final String careInstructions;
+  final double health;
 
-  Plant({
-    required this.name,
-    required this.imageUrl,
-  });
+  Plant(
+      {required this.name,
+      required this.imageUrl,
+      required this.wateringSchedule,
+      required this.careInstructions,
+      required this.health});
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:watery/auth.dart';
-import 'package:watery/models/plant.dart'; // Import your Plant model here
+import 'package:watery/models/plant.dart';
+
+import '../plant_details.dart'; // Import your Plant model here
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -9,10 +11,34 @@ class HomePage extends StatelessWidget {
   final User? user = Auth().currentUser;
 
   final List<Plant> userPlants = [
-    Plant(name: 'Rose', imageUrl: 'images/plant.jpg'),
-    Plant(name: 'Lavender', imageUrl: 'images/plant.jpg'),
-    Plant(name: 'Sunflower', imageUrl: 'images/plant.jpg'),
-    Plant(name: 'Cactus', imageUrl: 'images/plant.jpg'),
+    Plant(
+      name: 'Rose',
+      imageUrl: 'images/plant.jpg',
+      health: 0.8,
+      wateringSchedule: 'Twice a week',
+      careInstructions: 'Keep in direct sunlight',
+    ),
+    Plant(
+      name: 'Lavender',
+      imageUrl: 'images/plant.jpg',
+      health: 0.5,
+      wateringSchedule: 'Once a week',
+      careInstructions: 'Water lightly',
+    ),
+    Plant(
+      name: 'Sunflower',
+      imageUrl: 'images/plant.jpg',
+      health: 1,
+      wateringSchedule: 'Every other day',
+      careInstructions: 'Requires lots of sunlight',
+    ),
+    Plant(
+      name: 'Cactus',
+      imageUrl: 'images/plant.jpg',
+      health: 0.2,
+      wateringSchedule: 'Once a month',
+      careInstructions: 'Avoid overwatering',
+    ),
   ];
 
   Future<void> signOut() async {
@@ -35,11 +61,40 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
         title: _title(),
         actions: [
           IconButton(
             onPressed: () => signOut(),
             icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('Add new button clicked');
+        },
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.green,
+        fixedColor: Colors.white,
+        currentIndex:
+            0, // Current index of the selected tab (default is the first tab)
+        onTap: (int index) {
+          // Add functionality to switch between tabs
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Profile',
           ),
         ],
       ),
@@ -49,9 +104,6 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(
-              'Welcome, ${_userUid()}',
-            ),
             const SizedBox(
               height: 20,
             ),
@@ -64,22 +116,61 @@ class HomePage extends StatelessWidget {
                     physics: const NeverScrollableScrollPhysics(),
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 3,
+                            crossAxisCount: 2,
                             crossAxisSpacing: 10,
                             mainAxisSpacing: 10),
                     itemCount: userPlants.length,
                     itemBuilder: (context, index) {
-                      return Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(userPlants[index].imageUrl),
-                                fit: BoxFit.cover)),
+                      Plant plant = userPlants[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    PlantDetailsScreen(plant: plant),
+                              ));
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              plant.imageUrl,
+                              fit: BoxFit.cover,
+                            ),
+                            Text(
+                              plant.name,
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                            PlantHealthBar(value: plant.health),
+                          ],
+                        ),
                       );
                     },
                   )
           ],
         ),
       ),
+    );
+  }
+}
+
+class PlantHealthBar extends StatelessWidget {
+  final double value; // Value between 0 and 1 representing the health status
+  final Color color;
+
+  const PlantHealthBar({
+    super.key,
+    required this.value,
+    this.color = Colors.green,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearProgressIndicator(
+      value: value,
+      backgroundColor: color.withOpacity(0.5),
+      valueColor: AlwaysStoppedAnimation<Color>(color),
     );
   }
 }
